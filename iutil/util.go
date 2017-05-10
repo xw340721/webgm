66aeab2293e482fb5dfda12ca722d183249edb17
xw340721 <xw340721@163.com>
2017-05-10 11:57:52 +0800

    添加一个递归解析的反射方法

--- iutil/util.go
+++ iutil/util.go
@@ -3,6 +3,7 @@ package iutil
 import (
 	"encoding/base64"
 	"errors"
+	"reflect"
 	"strings"
 
 	"github.com/gogap/logrus"
@@ -27,4 +28,22 @@ func StringUpperIndex(s string) (string, error) {
 		return s, nil
 	}
 	return "", StringUpperIndexError
+}
+
+func DeepField(iface interface{}) []reflect.Value {
+	fields := make([]reflect.Value, 0)
+	ifv := reflect.ValueOf(iface)
+	ift := reflect.TypeOf(iface)
+
+	for i := 0; i < ift.NumField(); i++ {
+		v := ifv.Field(i)
+
+		switch v.Kind() {
+		case reflect.Struct:
+			fields = append(fields, DeepField(v.Interface())...)
+		default:
+			fields = append(fields, v)
+		}
+	}
+	return fields
 }
